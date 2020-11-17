Fixing weighted average

code/word2vec_gensim.py
@@ -78,11 +78,15 @@ def get_weighted_vector(word_vectors, word, main_list, morph_forms_dict):
     all_counts = np.asarray(list(count_dict.values()))
     vector_weights = np.divide(all_counts, total_count)
     print(vector_weights.shape)
-    
+
     all_vectors = np.asarray(list(vector_dict.values()))
     print(all_vectors.shape)
     bp()
     weighted_vector = np.average(all_vectors, weights=vector_weights)
+
+    print(weighted_vector)
+    print(weighted_vector.shape)
+    bp()
     return weighted_vector
 
 