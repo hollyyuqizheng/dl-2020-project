9020e914068c7cfd4b31e90532e3c0c2707022a6
ANDY <knoblock.andy@gmail.com>
2020-11-20 12:40:59 -1000

    Updated download

--- download.sh
+++ download.sh
@@ -1,5 +1,19 @@
 mkdir data
 cd data
-wget -nc https://www.dropbox.com/s/6urc128kepdhovr/nyt-paras.tsv
 
-python3 ../code/nyt_process.py
+
+fileid="1rSP8ZOp_gq7lRifsq73KXZXvq_CTtvkT"
+filename="nyt-data.tar.gz"
+cookie_file=".cookie"
+
+if [[ ! -f $filename ]]
+then
+	# get the file, save the cookies cuz apparently that is necessary
+	wget --save-cookies $cookie_file "https://drive.google.com/uc?export=download&id=$fileid" -O /dev/null
+	code="$(awk '/_warning_/ {print $NF}' $cookie_file)"
+	wget --load-cookies $cookie_file "https://drive.google.com/uc?export=download&confirm=$code&id=$fileid" -O $filename
+	rm $cookie_file
+	
+	# untar the file
+	tar -v -xzf $filename
+fi
